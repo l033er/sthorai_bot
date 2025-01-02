b0e212bf58dcdfc6681d30570a064838386fe838
l033er <yakelmi@yandex.ru>
2025-01-02 21:28:15 +0300

    upd

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ dp = Dispatcher()
 @dp.message(CommandStart())
 async def cmd_start(message: Message):
     await message.answer('Привет!')
-
-
+    
+    
 async def main():
     await dp.start_polling(bot)
-    
-    
+
+
 if __name__ == '__main__':
     asyncio.run(main())
     
