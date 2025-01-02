3d73c458a64851a22ad2a23f261ed269a628df51
l033er <yakelmi@yandex.ru>
2025-01-02 21:17:41 +0300

    upd 0.0.2

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ from aiogram import Bot, Dispatcher
 bot = Bot(token='7522553961:AAE9XKZNHTKau4R47UEnLO4ZS0FeehS6qjE')
 dp = Dispatcher()
 
+
 async def main():
     await dp.start_polling(bot)
+    
     
 if __name__ == '__main__':
     asyncio.run(main())
