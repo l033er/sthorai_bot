ae85cf994a5e0d3cc51992b5e58ceb786265838d
l033er <yakelmi@yandex.ru>
2025-01-02 21:26:47 +0300

    upd

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import asyncio
 
 from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import Message
 
 bot = Bot(token='7522553961:AAE9XKZNHTKau4R47UEnLO4ZS0FeehS6qjE')
 dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def cmd_start(message: Message):
+    await message.answer('Привет!')
 
 
 async def main():
