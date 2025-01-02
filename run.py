aa17e58adcea87de065d485ffb63090ed9634ba0
l033er <yakelmi@yandex.ru>
2025-01-02 21:50:47 +0300

    upd

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import asyncio
+from loguru import logger
 
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 
@@ -9,16 +10,16 @@ from config import TOKEN
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
-
 @dp.message(CommandStart())
 async def cmd_start(message: Message):
     await message.answer('Привет!')
-    
-    
+
 async def main():
     await dp.start_polling(bot)
 
-
 if __name__ == '__main__':
-    asyncio.run(main())
-    
+    logger.add("file.log", level="INFO") 
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        logger.info('Stopping the bot...')
