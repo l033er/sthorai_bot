b682ef4e94d7732f50194cc6e4040f52d80200f7
l033er <yakelmi@yandex.ru>
2025-01-02 22:10:43 +0300

    ✨ upd: create .env | delete config.py ✨

--- run.py
+++ run.py
@@ -1,27 +1,32 @@
 import asyncio
-
+import os
+from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 
-from config import TOKEN
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
-@dp.message(Command('help'))
-async def get_help(message: Message):
-    await message.answer(1)
+class CommandHandler:
+    @staticmethod
+    @dp.message(Command('help'))
+    async def get_help(message: Message):
+        await message.answer('Это команда хелп! 🎋')
 
-@dp.message(CommandStart())
-async def cmd_start(message: Message):
-    await message.answer('Это команда хелп')
+    @staticmethod
+    @dp.message(CommandStart())
+    async def cmd_start(message: Message):
+        await message.answer('Привет! 👋')
 
 async def main():
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
     try:
-        asyncio.run(main())
+        asyncio.run(main()) 
     except KeyboardInterrupt:
-        print('Бот остановлен. ✋')
+        print('Бот остановлен. ✋') 
